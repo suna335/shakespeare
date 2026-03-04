3f4c7f1b17b52ae330236ee5f69e5dd3fdb5cca7
niwa335 <mutaniwa@outlook.com>
2026-03-05 01:54:00 +0900

    ハンバーガーメニュー

--- js/common.js
+++ js/common.js
@@ -1,41 +1,37 @@
-function buttonClick(){
-    alert('ボタンがクリックされました');
-    return;
+let body = document.querySelector('body');
+let hamBtn = document.querySelector('.hamburger-btn');
+let navCon = document.querySelector('.glnav-content');
+let navClose = document.querySelector('.glnav-close');
+
+function glnavShowing() {
+    navCon.classList.replace('glnav-content', 'glnav-content-showing');
+    // gnavCon.setAttribute('aria-expanded', 'false');
+    // gnavCon.setAttribute("aria-hidden", "true");
+    body.classList.replace('scroll', 'no-scroll');
+    navClose.addEventListener("click", function () {
+        navCon.classList.replace('glnav-content-showing', 'glnav-content');
+        body.classList.replace('no-scroll', 'scroll');
+    });
 }
+    hamBtn.addEventListener('click', glnavShowing);
 
-let button = document.getElementById('testbt');
-button.addEventListener('click', buttonClick);
+    // function funcClose(btn) {
+    //       btn.setAttribute('aria-expanded', 'false');
+    //       btn.setAttribute('aria-hidden', 'true');
+    // }
 
-// function hamburger() {
-//   alert("クリックされました");
-//   console.log("クリックされました");
-//   return;
-// };
+    // glnavBtn.addEventListener("click", setAriaHidden);
 
-// let glnavBtn = document.querySelector('.hamburger-btn');
-// let glnavContent = document.querySelector('.glnav-content');
+    // // 背景クリック時
+    // glnavBtn.addEventListener('click', function () {
 
-// function setAriaHidden(btn) {
-//   // glnavContent.setAttribute("aria-hidden", "true");
-//   return;
-// }
+    //   funcClose(glnavBtn);
 
-// setAriaHidden(document.querySelector('.hamburger-btn'));
+    //   if (!flgPc) {
+    //     funcClose(glnavBtn);
+    //     document.body.style.overflow = 'visible';
+    //   }
+    // });
 
-// function funcClose(btn) {
-//       btn.setAttribute('aria-expanded', 'false');
-//       btn.setAttribute('aria-hidden', 'true');
-// }
-
-// glnavBtn.addEventListener("click", setAriaHidden);
-
-// // 背景クリック時
-// glnavBtn.addEventListener('click', function () {
-
-//   funcClose(glnavBtn);
-
-//   if (!flgPc) {
-//     funcClose(glnavBtn);
-//     document.body.style.overflow = 'visible';
-//   }
-// });
+    // テスト用
+    // alert('ボタンがクリックされました');
