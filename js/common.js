318236e415f16111d75e363566bcbc9217d92f56
niwa335 <mutaniwa@outlook.com>
2026-03-07 01:41:33 +0900

    おつかれさま

--- js/common.js
+++ js/common.js
@@ -1,33 +1,50 @@
 let page = document.querySelector('.page');
-let body = document.querySelector('.sbody');
+let body = document.querySelector('.body');
 let hamBtn = document.querySelector('.hamburger-btn');
-let nav = document.querySelector('.glnav');
-let navClose = document.querySelector('.glnav-close');
+let nav = document.querySelector('.gnav');
+let navClose = document.querySelector('.gnav-close');
+
+// let window = document.querySelector('window');
+// let header = document.querySelector('.bHeader');
+// let scrollHeight = header.outerHeight();
+// let main = document.querySelector('.bMain');
+// let headerTop = header.offset().top;
+
+//   function handleScroll() {
+//     var value = window.scrollTop();
+//     if ( value > headerTop ) {
+//       header.classList.add('hyde');
+//       main.style.marginTop = scrollHeight + 'px';
+//     } else {
+//       header.classList.remove('hyde');
+//       main.style.marginTop = '0';
+//     }
+//   }
+
+//   handleScroll();
+//   window.addEventListener('scroll', handleScroll);
 
 // グローバルナビゲーションの表示と非表示
-function glnavShowing() {
-    nav.classList.replace('glnav', 'glnav-showing');
+function gnavShowing() {
+    nav.classList.replace('gnav', 'gnav-showing');
     page.classList.add('no-scroll');
     body.classList.add('no-scroll');
 
-    navClose.addEventListener("click", glnavclosing);
+    navClose.addEventListener("click", gnavclosing);
 }
 
-function glnavclosing() {
-    nav.classList.replace('glnav-showing', 'glnav');
+function gnavclosing() {
+    nav.classList.replace('gnav-showing', 'gnav');
     page.classList.remove('no-scroll');
     body.classList.remove('no-scroll');
 }
-hamBtn.addEventListener('click', glnavShowing);
+hamBtn.addEventListener('click', gnavShowing);
 
 
-// 画面幅が750px以下に変更された時、グローバルナビゲーションを閉じる
-const mql = window.matchMedia('(max-width: 750px)');
+// 画面幅が広くなったらＧナビを閉じる
+const mql = window.matchMedia('(max-width: 650px)');
 const initNav = function () {
-    glnavclosing();
+    gnavclosing();
 };
 mql.addEventListener('change', initNav);
 
-
-// テスト用
-// alert('ボタンがクリックされました');
