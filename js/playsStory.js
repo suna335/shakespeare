d73cfe994b2a44d82f6d9c792e8d820e9fbe8c14
niwa335 <mutaniwa@outlook.com>
2026-03-17 12:53:18 +0900

    スクロールもどったお！

--- js/playsStory.js
+++ js/playsStory.js
@@ -9,6 +9,9 @@ const titusAndronicus = document.querySelector(".titusAndronicus-story");
 const juliusCaesar = document.querySelector(".juliusCaesar-story");
 const romeoAndJuliet = document.querySelector(".romeoAndJuliet-story");
 
+let hamletCheckBox = document.querySelector(".readmore-checkbox-hamlet");
+let kingLearCheckBox = document.querySelector(".readmore-checkbox-kingLear");
+
 // テキストファイル外部読み込み
 async function story(className, fileName) {
   let response = await fetch("txt/" + fileName + ".txt");
@@ -29,3 +32,22 @@ if (titusAndronicus) {
   story(juliusCaesar, "juliusCaesar");
   story(romeoAndJuliet, "romeoAndJuliet");
 }
+
+// アコーディオンUI対応
+function accordion(e) {
+  if (!e.currentTarget.checked) {
+    this.name.scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+}
+
+hamletCheckBox.addEventListener("change", {
+  name: hamlet,
+  handleEvent: accordion,
+});
+
+kingLearCheckBox.addEventListener("change", {
+  name: kingLear,
+  handleEvent: accordion,
+});
